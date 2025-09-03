4309192766dc5c90d383f2fa0e57ffa27d6c9686
H82M19\Student <Student>
2025-09-03 08:43:10 +0700

    tuan 3

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Layout1 from './components/layout1';
+import Layout2 from './components/layout2';
+import Layout3 from './components/layout3';
+import Layout4 from './components/layout4';
+import Layout5 from './components/layout5';
+import Layout6 from './components/layout6';
+import Layout7 from './components/layout7';
+import Layout8 from './components/layout8';
+
+export default function App() {
+  return (
+    // <Layout1/>
+    // <Layout2/>
+    // <Layout3/>
+    // <Layout4/>
+    // <Layout5/>
+    // <Layout6/>
+    // <Layout7/>
+    <Layout8/>
+  );
+}
